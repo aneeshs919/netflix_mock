f70f810635005be5bc519670342cc25addcba82e
“aneeshs919” <aneeshs919@gmail.com>
2023-11-22 11:10:31 +0530

    Mobile responsive fix WIP

--- src/pageComponents/index.tsx
+++ src/pageComponents/index.tsx
@@ -27,29 +27,30 @@ const DETAILS_DATA_STRUCTURE = {
     released: '2023'
 }
 
-const HeroDetails = ({title, overview, rating}) => {
+const HeroDetails = ({ title, overview, rating }) => {
     return (
-        <div className='relative h-[550px] flex items-bottom items-end'>
+        <div className='relative flex h-[200px] w-[360px] lg:w-full m-auto lg:h-[500px] items-bottom items-end'>
             <div className='max-h-[550px] overflow-hidden bg-black absolute w-full'>
-                <Image className='w-full h-[100%] opacity-50' src={'/hero.webp'} width={'1200'} height={'300'} alt='hero_image' />
+                <Image className='w-full h-[100%] opacity-50 rounded-md' src={'/hero.webp'} width={'1200'} height={'300'} alt='hero_image' />
             </div>
-            <div className='z-10 relative p-14 w-3/5'>
-                <div className='text-5xl mb-6 font-weight'>{DETAILS_DATA_STRUCTURE.title}</div>
-                <div className='text-xl'>{DETAILS_DATA_STRUCTURE.description}</div>
-                <div className='flex gap-4 mt-4 text-gray-400'>
+            <div className='z-10 relative p-4 lg:p-14 w-3/5'>
+                <div className='text-base lg:text-5xl mb-2 lg:mb-6 font-weight'>{DETAILS_DATA_STRUCTURE.title}</div>
+                {/* <div className='text-xl'>{DETAILS_DATA_STRUCTURE.description}</div> */}
+                <div className='gap-4 mt-4 text-gray-400 hidden lg:flex'>
                     <div className='text-xl'>{DETAILS_DATA_STRUCTURE.runTime} min</div>
                     <div className='text-xl'>{DETAILS_DATA_STRUCTURE.released}</div>
                 </div>
-                <div className='flex text-xl capitalize gap-4 mt-4'>{DETAILS_DATA_STRUCTURE.tag.map(item => <div key={item}>{item} .</div>)}</div>
-                <div className='mt-8'>
+                <div className='text-xl capitalize gap-4 mt-4 hidden lg:flex'>{DETAILS_DATA_STRUCTURE.tag.map(item => <div key={item}>{item} .</div>)}</div>
+                <div className=' mt-2 lg:mt-8'>
                     <div className='flex gap-4 items-center'>
-                        <div className='flex justify-center items-center gap-4 text-2xl'>
-                            <div className='bg-white w-20 h-20 rounded-full flex justify-center items-center hover:scale-110 hover:delay-75 duration-300'>
-                                <Image src={'/play.png'} width={'35'} height={'35'} alt='play' />
+                        <div className='flex justify-center items-center gap-4 '>
+                            <div className='bg-white flex justify-center rounded-sm p-2 px-3 gap-2 text-black items-center hover:scale-110 hover:delay-75 duration-300'>
+                                <Image src={'/play.png'} width={'20'} height={'20'} alt='play' />
+                                <div>Play</div>
+
                             </div>
-                            <div>Play</div>
                         </div>
-                        <div className='bg-gray-600 w-14 h-14 rounded-full flex justify-center items-center hover:scale-110 hover:delay-75 duration-300'>
+                        <div className='bg-gray-600 w-14 h-14 rounded-full hidden lg:flex justify-center items-center hover:scale-110 hover:delay-75 duration-300'>
                             <Image src={'/plus.png'} width={'30'} height={'30'} alt='wishlist' />
                         </div>
                     </div>
@@ -69,9 +70,8 @@ const Title = (props: { title: string }) => {
 const Browse = ({ popular }: { popular: dataType[] }) => {
     const router = useRouter()
     const randomDigit = Math.floor(Math.random() * 10);
-    const banner = popular[randomDigit].banner
-
-    console.log('popular333', popular)
+    const banner = popular[randomDigit]
+    console.log('popular333', banner)
     return (
         <div className='m-auto'>
             <HeroDetails />
